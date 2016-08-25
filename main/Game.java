3247ac4cf428a106266c089dcfba25e850085264
Drew Funderburk <thatfunderburkguy@gmail.com>
2016-08-25 12:12:16 -0500

    Added GamePanel and began setting up window

--- main/Game.java
+++ main/Game.java
@@ -4,7 +4,16 @@ import javax.swing.JFrame;
 
 public class Game extends JFrame {
 
+   public Game() {
+      setTitle("Knight Slayer Jorig");
+      setContentPane(new GamePanel());
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setResizable(false);
+      pack();
+      setVisible(true);
+   }
+   
    public static void main(String[] args) {
-
+      new Game();
    }
 }
---
+++ main/GamePanel.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class GamePanel extends JFrame implements Runnable, ActionListener {
+
+   // Gets screen resolution and stores it
+   public static Dimension screenSize = new Dimension(
+         Toolkit.getDefaultToolkit().getScreenSize());
+   
+   public GamePanel() {
+      
+   }
+
+   @Override
+   public void actionPerformed(ActionEvent arg0) {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void run() {
+      // TODO Auto-generated method stub
+
+   }
+
+}
